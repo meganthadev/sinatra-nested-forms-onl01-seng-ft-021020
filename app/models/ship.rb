e070a8296893eca34475e3d8227f6ead86f444a7
Megan Horsfield <meganthadev@gmail.com>
2020-04-09 00:42:23 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,23 @@
 class Ship
+  
+   attr_reader :name, :type, :booty 
+  
+  @@all = []
+  
+  def intialize(name, weight, height)
+    @name = :name 
+    @weight = :weight
+    @height = :height
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end   
+  
+  def self.clear
+    @@all.destroy
+  end   
+  
+  
 end
