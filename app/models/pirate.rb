029b037284a3b10a54fb4683b20227aa6088c194
Megan Horsfield <meganthadev@gmail.com>
2020-04-09 01:21:14 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,19 +1,15 @@
 class Pirate
-  
-  attr_reader :name, :weight, :height 
-  
-  @@all = []
-  
-  def intialize(name, weight, height)
-    @name = :name 
-    @weight = :weight
-    @height = :height
-    @@all << self
-  end 
-  
-  def self.all 
-    @@all 
-  end   
-  
-  
-end
+	  attr_reader :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
