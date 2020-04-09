4bbf50f2c7e291dbac8636b8031c91696be161f8
Megan Horsfield <meganthadev@gmail.com>
2020-04-09 01:20:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,19 +10,17 @@ module FormsLab
     get '/new' do
       erb :'pirates/new'
     end
-  
-  post '/pirates' do
-    @pirate = Pirate.new(params[:pirate])
-    
-    params[:pirate][:ships].each do |details|
-      Ship.new(details)
-    end 
-    @ships = Ship.all 
-    
-    erb :'pirates/show' 
-  end
- end 
 
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
 
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
+  end 
   
-end 
+end     
