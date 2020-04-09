c2ca904494d87ed1f576981d53be8fc1ae73e04a
Megan Horsfield <meganthadev@gmail.com>
2020-04-09 01:06:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,10 +3,13 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
+   get '/' do
+      erb :root
+    end
 
-  get '/new' do 
-    erb :new 
-  end 
+    get '/new' do
+      erb :'pirates/new'
+    end
   
   post '/pirates' do
     @pirate = Pirate.new(params[:pirate])
